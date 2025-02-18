528c57478491e1e8991157b25067dbfdf8bbc044
qzda <qinzhidaoa@gmail.com>
2025-02-18 22:03:20 +0800

    chore: minify template

--- packages/web-component/vite.config.ts
+++ packages/web-component/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import fs from "node:fs";
+import { IsDev } from "./src/utils";
 
 const virtualModuleId = "virtual:template";
 
@@ -16,10 +17,24 @@ export default defineConfig({
       load(id) {
         if (id === "\0" + virtualModuleId) {
           const content = fs.readFileSync("src/template.html", "utf-8");
-          return {
-            code: `export default \`${content}\``,
-            map: null,
-          };
+
+          if (IsDev) {
+            return {
+              code: `export default \`${content}\``,
+              map: null,
+            };
+          } else {
+            let _content = content.replaceAll("\r", "").replaceAll("\n", " ");
+
+            while (_content.includes("  ")) {
+              _content = _content.replaceAll("  ", " ");
+            }
+
+            return {
+              code: `export default \`${_content}\``,
+              map: null,
+            };
+          }
         }
       },
 
